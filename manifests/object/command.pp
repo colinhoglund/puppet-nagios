67f6d329feda316c7fa2b3c2782952a435c79ea7
Colin Hoglund <colin.hoglund@nyu.edu>
2015-06-03 18:08:23 -0400

    adding command defined type

--- manifests/object/command.pp
+++ manifests/object/command.pp
@@ -1,5 +1,11 @@
 #
 define nagios::object::command(
+  $command_line
 ){
-
+  include nagios
+  nagios_command { $title:
+    ensure       => present,
+    command_line => $command_line,
+    target       => $nagios::config::command_config_file,
+  }
 }
