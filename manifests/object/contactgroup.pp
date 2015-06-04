8f4557ba6f6508c15a676dff9d92293edd3ebdb8
Colin Hoglund <colin.hoglund@nyu.edu>
2015-06-04 09:12:28 -0400

    adding contactgroup defined type

--- manifests/object/contactgroup.pp
+++ manifests/object/contactgroup.pp
@@ -1,2 +1,31 @@
 #
-define nagios::object::contactgroup(){}
+define nagios::object::contactgroup(
+  $contactgroup_name    = $title,
+  $ensure               = 'present',
+  $contactgroup_alias   = undef,
+  $contactgroup_members = undef,
+  $group                = undef,
+  $members              = undef,
+  $mode                 = undef,
+  $owner                = undef,
+  $provider             = undef,
+  $register             = undef,
+  $target               = $nagios::config::contactgroup_config_file,
+  $use                  = undef,
+){
+  include nagios
+
+  nagios_contactgroup { $contactgroup_name:
+    ensure               => $ensure,
+    alias                => $contactgroup_alias,
+    contactgroup_members => $contactgroup_members,
+    group                => $group,
+    members              => $members,
+    mode                 => $mode,
+    owner                => $owner,
+    provider             => $provider,
+    register             => $register,
+    target               => $target,
+    use                  => $use,
+  }
+}
